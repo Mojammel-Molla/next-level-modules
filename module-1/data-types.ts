eacb03f6f537cab9634df5def4c2b3f2c2a0e9f9
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-05-11 23:47:15 +0600

    Basic data types

---
+++ module-1/data-types.ts
@@ -0,0 +1,23 @@
+// Basic Data types
+
+// string
+let firstName: string = 'Babul Bhai';
+
+// number
+let age: number = 69;
+
+// boolean
+let isAdmin: boolean = true;
+
+// undefined
+let x: undefined = undefined;
+
+// null
+let y: null = null;
+
+let d: number;
+d = 123;
+
+// array
+let friends: string[] = ['Abul', 'Kalam', 'Azad'];
+let thereAges: number[] = [45, 69, 56];
